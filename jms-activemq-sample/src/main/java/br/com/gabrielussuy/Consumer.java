3b0a71755fbd9fb119047c3e6a115b4a607f282f
Gabriel Ussuy <gabriel_ussuy95@hotmail.com>
2021-08-05 22:18:41 -0300

    creation of consumer class

---
+++ jms-activemq-sample/src/main/java/br/com/gabrielussuy/Consumer.java
@@ -0,0 +1,22 @@
+package br.com.gabrielussuy;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Scanner;
+
+public class Consumer {
+    public static void main(String[] args) throws NamingException, JMSException {
+        InitialContext context = new InitialContext();
+
+        ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+        Connection connection = factory.createConnection();
+        connection.start();
+
+        new Scanner(System.in).nextLine();
+        connection.close();
+        context.close();
+    }
+}
