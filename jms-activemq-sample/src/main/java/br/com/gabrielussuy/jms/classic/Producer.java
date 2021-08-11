46a6175f5f4f32c8f416adcc3797cb52e4e49c3d
Gabriel Ussuy <gabriel_ussuy95@hotmail.com>
2021-08-10 21:34:28 -0300

    Producer

---
+++ jms-activemq-sample/src/main/java/br/com/gabrielussuy/jms/classic/Producer.java
@@ -0,0 +1,27 @@
+package br.com.gabrielussuy.jms.classic;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class Producer {
+
+    private static final String QUEUE_NAME = "financeiro";
+    private static final String MESSAGE = "<pedido><id>123</id></pedido>";
+
+    public static void main(String[] args) throws NamingException, JMSException {
+        InitialContext context = new InitialContext();
+
+        ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+        Connection connection = factory.createConnection();
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination queue = (Destination) context.lookup(QUEUE_NAME);
+
+        MessageProducer producer = session.createProducer(queue);
+        Message message = session.createTextMessage(MESSAGE);
+
+        producer.send(message);
+    }
+}
