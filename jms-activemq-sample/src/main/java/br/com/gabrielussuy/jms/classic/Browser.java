f64320806dea0bfcf2037cacc1795981ccb7898a
Gabriel Ussuy <gabriel_ussuy95@hotmail.com>
2021-08-11 21:41:03 -0300

    browser sample

---
+++ jms-activemq-sample/src/main/java/br/com/gabrielussuy/jms/classic/Browser.java
@@ -0,0 +1,29 @@
+package br.com.gabrielussuy.jms.classic;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Enumeration;
+
+public class Browser {
+
+    private static final String QUEUE_NAME = "financeiro";
+
+    public static void main(String[] args) throws NamingException, JMSException {
+        InitialContext context = new InitialContext();
+
+        ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+        Connection connection = factory.createConnection();
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination fila = (Destination) context.lookup(QUEUE_NAME);
+        QueueBrowser browser = session.createBrowser((Queue) fila);
+
+        Enumeration msgs = browser.getEnumeration();
+        while (msgs.hasMoreElements()) {
+            TextMessage msg = (TextMessage) msgs.nextElement();
+            System.out.println("Message: " + msg.getText());
+        }
+    }
+}
