ac30ee0433ca520ba618f14bc3d04bee66c02f91
Gabriel Ussuy <gabriel_ussuy95@hotmail.com>
2021-08-16 21:57:23 -0300

    Auth config for activemq

---
+++ jms-activemq-sample/src/main/java/br/com/gabrielussuy/jms/classic/topic/TopicSelectorSample.java
@@ -0,0 +1,42 @@
+package br.com.gabrielussuy.jms.classic.topic;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Scanner;
+
+public class TopicConsumerSample {
+
+    private static final String TOPIC_NAME = "store";
+    private static final String CLIENT_ID = "inventory";
+    private static final String SIGNATURE = "signature";
+
+    public static void main(String[] args) throws NamingException, JMSException {
+        InitialContext context = new InitialContext();
+        ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+
+        Connection connection = factory.createConnection();
+        connection.setClientID(CLIENT_ID);
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Topic  topic = (Topic ) context.lookup(TOPIC_NAME);
+
+        MessageConsumer consumer = session.createDurableSubscriber(topic, SIGNATURE);
+
+        consumer.setMessageListener(message -> {
+            TextMessage textMessage = (TextMessage) message;
+            try {
+                System.out.println(textMessage.getText());
+            } catch (JMSException e) {
+                e.printStackTrace();
+            }
+        });
+
+        new Scanner(System.in).nextLine();
+
+        session.close();
+        connection.close();
+        context.close();
+    }
+}
